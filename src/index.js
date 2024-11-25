51f28d5830b61ee922c0934e41357b86cb2fb4f9
MateoVGarcia <mateovgarcia2003@gmail.com>
2024-11-25 16:21:45 -0300

    Integración CORS Hotfix#3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
 import { PORT } from "./config.js";
+import cors from "cors";
 
 const app = express();
 
