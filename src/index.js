4e71aa0559204ebffb2eb3f84f735daee5f0d09c
MateoVGarcia <mateovgarcia2003@gmail.com>
2024-11-25 16:31:04 -0300

    Integración CORS Hotfix#5

--- src/index.js
+++ src/index.js
@@ -1,18 +1,33 @@
 import express from "express";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
+import cors from "cors";
 import { PORT } from "./config.js";
 
 const app = express();
 
+// Middlewares
 app.use(morgan("dev"));
 
-// middlewares
+// Configuración de CORS para permitir solicitudes desde el dominio de Netlify
+const allowedOrigins = ["https://idyllic-pothos-c76aef.netlify.app"];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use()
+
+// Rutas
 app.use(usersRoutes);
 
-app.listen(PORT);
-// eslint-disable-next-line no-console
-console.log("Server on port", PORT);
+// Inicio del servidor
+app.listen(PORT, () => {
+  console.log("Server on port", PORT);
+});
