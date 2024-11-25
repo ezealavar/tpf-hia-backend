f7f5131cc26cec6e8af23a82aef16fa50da74450
MateoVGarcia <mateovgarcia2003@gmail.com>
2024-11-25 16:25:34 -0300

    Integración CORS Hotfix#4

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
 import { PORT } from "./config.js";
-import cors from "cors";
 
 const app = express();
 
@@ -13,7 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use()
 app.use(usersRoutes);
-app.use(cors());
 
 app.listen(PORT);
 // eslint-disable-next-line no-console
